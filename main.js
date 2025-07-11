46d529d8b1d45d61c23e9dd750961a4376e64131
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-11 23:55:11 +0100

    checking if the inputed email contain @

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const textInput = document.querySelector("input[type=text]");
-
+const submitBTn = document.querySelector('.submit')
 
 textInput.addEventListener('keyup', runEvent);
+submitBTn.addEventListener('click', submit)
 
 function runEvent(e){
     var emailInput = e.target.value.toLowerCase()
     
+}
+
+function submit(e){
+    e.preventDefault()
+    if(emailInput.includes('@')){
+        console.log('the email is valid');
+        
+    }else {
+        console.log("invalid")
+    }
 }
