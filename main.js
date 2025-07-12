1aee6540a6ee26721c7781f0063f3ee850997a89
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-12 22:21:29 +0100

    coding the js

--- main.js
+++ main.js
@@ -4,21 +4,3 @@ const submitBTn = document.querySelector('.submit')
 textInput.addEventListener('keyup', runEvent);
 submitBTn.addEventListener('click', submit)
 
-function runEvent(e){
-    var emailInput = e.target.value.toLowerCase()
-    
-}
-
-function containAtSymbol(text){
-    return text.includes('@');
-}
-
-function submit(e){
-    e.preventDefault()
-    if(containAtSymbol(emailInput)){
-        console.log('the email is valid');
-        
-    }else {
-        console.log("invalid")
-    }
-}
